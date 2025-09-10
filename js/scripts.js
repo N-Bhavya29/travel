7963fd416dcf7ca1b89195c46243a49cfbca0718
abhishekthacker432 <abhishek@innovatrix.agency>
2025-09-10 21:27:48 +0530

    Changes

--- js/scripts.js
+++ js/scripts.js
@@ -146,7 +146,7 @@
 
 
 		// SLIDER
-		var menu = ['WHO WE ARE<span>OUR PASSION</span>', 'NOTHERN LIGHTS<span>SPECIAL OFFER</span>', 'BEST SELLING<span>ICE CLIMBING</span>', 'THE FAMOUS<span>HIKING TRAILS</span>'];
+		var menu = ['RANN UTSAV', 'ROAD TO HEAVEN', 'KHARI NADI', 'RYAN RESORT'];
 		var interleaveOffset = 0.5;
 		var swiperOptions = {
 			loop: true,
